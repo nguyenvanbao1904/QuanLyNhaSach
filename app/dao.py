8b6be7c1ea20437512d6bc630955c8132b348316
nguyenvanbao1904 <bao19042004@gmail.com>
2024-12-08 18:21:15 +0700

    fix handel err when login fail

--- app/dao.py
+++ app/dao.py
@@ -6,10 +6,12 @@ from app.models import User
 
 
 def check_login(username, password):
-    password = str(hashlib.md5(password.strip().encode("utf-8")).hexdigest())
-    return User.query.filter(User.username == username,
-                             User.password == password).first()
-
+    try:
+        password = str(hashlib.md5(password.strip().encode("utf-8")).hexdigest())
+        return User.query.filter(User.username == username,
+                                 User.password == password).first()
+    except Exception as e:
+        return None
 def add_user(username, password, first_name, last_name):
     try:
         user = User(username = username, password = str(hashlib.md5(password.strip().encode("utf-8")).hexdigest()), first_name = first_name, last_name = last_name)
