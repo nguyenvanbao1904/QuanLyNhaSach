a2447dca0b7bf3706be07d31320467c5eb6858c3
nguyenvanbao1904 <bao19042004@gmail.com>
2024-12-14 00:30:15 +0700

    apply decorator

--- app/index.py
+++ app/index.py
@@ -51,11 +51,11 @@ def login():
 
 
 @app.route("/logout")
-@role_required("khachHang")
+@role_required(['khachHang'])
 def logout():
-    print(1)
     logout_user()
     return redirect('/')
+
 
 @app.route("/signup", methods=["get", "post"])
 def signup():
@@ -80,32 +80,33 @@ def signup():
 
 
 @app.route('/cart', methods=['GET', 'POST'])
+@role_required(['khachHang'])
 def cart():
-    if current_user.is_authenticated:
-        if request.method == 'POST':
-            try:
-                my_cart = dao.create_cart(current_user.id)
-                product = request.form.to_dict()
-                product['order_id'] = my_cart.id
-                dao.add_product_in_cart(**product)
-                return jsonify({
-                    'success': True,
-                    'message': 'Item added to cart successfully'
-                }), 201
-            except Exception as e:
-                return jsonify({
-                    'success': False,
-                    'message': f'An error occurred: {str(e)}'
-                }), 500
+    if request.method == 'POST':
+        try:
+            my_cart = dao.create_cart(current_user.id)
+            product = request.form.to_dict()
+            product['order_id'] = my_cart.id
+            dao.add_product_in_cart(**product)
+            return jsonify({
+                'success': True,
+                'message': 'Item added to cart successfully'
+            }), 201
+        except Exception as e:
+            return jsonify({
+                'success': False,
+                'message': f'An error occurred: {str(e)}'
+            }), 500
 
-        my_carts = dao.get_cart_by_user_id(current_user.id)
-        total_price = 0
-        if my_carts:
-            total_price = dao.get_total_price(my_carts)
-        return render_template('cart.html', carts=my_carts, total_price=total_price)
-    return redirect(url_for('login'))
+    my_carts = dao.get_cart_by_user_id(current_user.id)
+    total_price = 0
+    if my_carts:
+        total_price = dao.get_total_price(my_carts)
+    return render_template('cart.html', carts=my_carts, total_price=total_price)
+
 
 @app.route('/delete/cart/<int:cart_id>', methods=['DELETE'])
+@role_required(['khachHang'])
 def delete_product_in_cart(cart_id):
     try:
         dao.delete_product_in_cart(cart_id, current_user.id)
@@ -126,8 +127,8 @@ def products_detail(product_id):
         return redirect(url_for('home'))
 
 
-@login_required
 @app.route('/update-cart', methods=['PATCH'])
+@role_required(['khachHang'])
 def update_cart():
     data = request.get_json()
     product_in_cart_id = data.get("product_in_cart_id")
@@ -146,61 +147,59 @@ def update_cart():
 
 
 @app.route('/checkout', methods=['GET'])
+@role_required(['khachHang'])
 def checkout():
-    if current_user.is_authenticated:
-        carts = dao.get_cart_by_user_id(current_user.id)
-        if carts is None:
-            return redirect(url_for('not_found_page'))
-        total_price = 0
-        if carts:
-            total_price = dao.get_total_price(carts)
-        return render_template('checkout_templates/checkout.html', carts=carts, total_price=total_price)
-    return redirect(url_for('login'))
+    carts = dao.get_cart_by_user_id(current_user.id)
+    if carts is None:
+        return redirect(url_for('not_found_page'))
+    total_price = 0
+    if carts:
+        total_price = dao.get_total_price(carts)
+    return render_template('checkout_templates/checkout.html', carts=carts, total_price=total_price)
 
 
 def checkout_method(method, ttl):
-    if current_user.is_authenticated:
-        order_id = request.args.get('order_id')
-        carts = dao.get_cart_by_user_id(current_user.id)
-        if carts is None:
-            return redirect(url_for('not_found_page'))
-        total_price = dao.get_total_price(carts)
-        carts.create_date = datetime.now()
-        deadline = carts.create_date + timedelta(seconds=ttl)
-        dao.change_status_order(carts, carts.create_date, OrderStatus.PROCESSING)
-        redis_utils.set_ttl_order(order_id, ttl, "PROCESSING")
-        return render_template(f'/checkout_templates/checkout_{method}.html', order_id=int(order_id),
-                               total_price=total_price, deadline=deadline)
+    order_id = request.args.get('order_id')
+    carts = dao.get_cart_by_user_id(current_user.id)
+    if carts is None:
+        return redirect(url_for('not_found_page'))
+    total_price = dao.get_total_price(carts)
+    carts.create_date = datetime.now()
+    deadline = carts.create_date + timedelta(seconds=ttl)
+    dao.change_status_order(carts, carts.create_date, OrderStatus.PROCESSING)
+    redis_utils.set_ttl_order(order_id, ttl, "PROCESSING")
+    return render_template(f'/checkout_templates/checkout_{method}.html', order_id=int(order_id),
+                           total_price=total_price, deadline=deadline)
 
 
 @app.route('/checkout/offline', methods=['GET'])
+@role_required(['khachHang'])
 def checkout_offline():
     return checkout_method("offline", 10)
 
 
 @app.route('/checkout/online', methods=['GET'])
+@role_required(['khachHang'])
 def checkout_online():
     return checkout_method("online", 60)
 
 
 @app.route('/checkout/confirm', methods=['POST'])
+@role_required(['khachHang'])
 def checkout_confirm():
-    if current_user.is_authenticated:
-        data = request.get_json()
-        order_id = data.get("order_id")
-        order = dao.get_order_by_id(order_id, current_user.id)
-        if order is None:
-            return jsonify({'success': False, 'message': 'order not found'})
-        dao.change_status_order(order, order.create_date, OrderStatus.DONE)
-        redis_client.delete(order_id)
-        return jsonify({'success': True, 'message': 'Checkout done'})
-    return jsonify({'success': False, 'message': 'Please login'})
+    data = request.get_json()
+    order_id = data.get("order_id")
+    order = dao.get_order_by_id(order_id, current_user.id)
+    if order is None:
+        return jsonify({'success': False, 'message': 'order not found'})
+    dao.change_status_order(order, order.create_date, OrderStatus.DONE)
+    redis_client.delete(order_id)
+    return jsonify({'success': True, 'message': 'Checkout done'})
 
 
 @login_manager.user_loader
 def load_user(user_id):
     return dao.get_user_by_id(user_id)
-
 
 if __name__ == '__main__':
     from app.admin import *
--- decorator.py
+++ decorator.py
@@ -1,13 +1,13 @@
 from functools import wraps
 from flask_login import current_user
 from flask import redirect, url_for, abort
-def role_required(role):
+def role_required(roles):
     def decorator(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
             if not current_user.is_authenticated:
                 return redirect(url_for('login'))
-            if current_user.account_role.value!= role:
+            if current_user.account_role.value not in roles:
                 return abort(403)
             return func(*args, **kwargs)
         return wrapper
